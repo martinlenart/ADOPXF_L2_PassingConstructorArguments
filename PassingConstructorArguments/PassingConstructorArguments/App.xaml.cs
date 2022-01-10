432dab9c0483af51300a16a3fcf95aa0b17df82f
Martin Lenart <martin_lenart@icloud.com>
2022-01-10 20:12:16 +0100

    Add project files.

---
+++ PassingConstructorArguments/PassingConstructorArguments/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PassingConstructorArguments
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Views.PassingConstructorArgumentsPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PassingConstructorArguments/PassingConstructorArguments/Views/PassingConstructorArgumentsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace PassingConstructorArguments.Views
+{
+	public partial class PassingConstructorArgumentsPage : ContentPage
+	{
+		public PassingConstructorArgumentsPage()
+		{
+			InitializeComponent();
+			
+			bw1.Color = new Color(0.9);
+			bw2.Color = Color.FromRgb(192, 75, 150);
+		}
+	}
+}
